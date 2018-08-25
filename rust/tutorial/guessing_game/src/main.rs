6113d0698f2091778d423b564765bab62aa9437a
Olivier Desenfans <desenfans.olivier@gmail.com>
2018-08-25 14:54:26 +0200

    [Rust] Improvements to the guessing game app

--- rust/tutorial/guessing_game/src/main.rs
+++ rust/tutorial/guessing_game/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     println!("Guess the number!");
 
     let secret_num = rand::thread_rng().gen_range(1, 101);
+    let mut nb_attempts: u32 = 0;
 
     loop {
         println!("Please input your guess:");
@@ -21,13 +22,14 @@ fn main() {
             Err(_) => continue,
         };
 
+        nb_attempts += 1;
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_num) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! ({} attempts)", nb_attempts);
                 break;
             }
         }
