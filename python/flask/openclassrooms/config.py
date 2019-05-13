f149d4fac012643ab5b267f60cf7b7293e9f2322
Olivier Desenfans <desenfans.olivier@gmail.com>
2019-05-13 15:32:56 +0200

    [Flask] OCR tutorial, up to part 2

---
+++ python/flask/openclassrooms/config.py
@@ -0,0 +1,6 @@
+# To generate a new secret key:
+# >>> import random, string
+# >>> "".join([random.choice(string.printable) for _ in range(24)])
+SECRET_KEY = '}U5\rJlL9S{%D.u\\"6bPwaq=!'
+
+FB_APP_ID = 553484131847572
