89c2f5009433414a371f092ca79a8fc12f63263b
Olivier Desenfans <desenfans.olivier@gmail.com>
2019-05-28 15:44:54 +0200

    [WIP] Saved progress on flask example

--- python/flask/openclassrooms/config.py
+++ python/flask/openclassrooms/config.py
@@ -1,6 +1,11 @@
 # To generate a new secret key:
 # >>> import random, string
 # >>> "".join([random.choice(string.printable) for _ in range(24)])
-SECRET_KEY = '}U5\rJlL9S{%D.u\\"6bPwaq=!'
 
+import os
+
+SECRET_KEY = '}U5\rJlL9S{%D.u\\"6bPwaq=!'
 FB_APP_ID = 553484131847572
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
